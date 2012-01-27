b26b6158e2d864124617291dd83634f3afd661a0
Kevin Sawicki <kevin@github.com>
2012-01-27 13:22:13 -0800

    Add refresh action to GaugeListActivity

--- src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
+++ src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
@@ -2,8 +2,12 @@ package com.github.mobile.gauges.ui;
 
 import android.R;
 import android.os.Bundle;
+import android.support.v4.view.Menu;
+import android.support.v4.view.MenuItem;
 
+import com.github.mobile.gauges.R.id;
 import com.github.mobile.gauges.R.layout;
+import com.github.mobile.gauges.R.menu;
 
 import roboguice.activity.RoboFragmentActivity;
 
@@ -12,17 +16,33 @@ import roboguice.activity.RoboFragmentActivity;
  */
 public class GaugeListActivity extends RoboFragmentActivity {
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu optionsMenu) {
+		getMenuInflater().inflate(menu.gauges, optionsMenu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case id.refresh:
+			((GaugeListFragment) getSupportFragmentManager().findFragmentById(
+					R.id.list)).refresh();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
+	}
+
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(layout.gauge_list);
 
-		GaugeListFragment gauges = (GaugeListFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.list);
-		if (gauges == null) {
-			gauges = new GaugeListFragment();
+		if (getSupportFragmentManager().findFragmentById(R.id.list) == null)
 			getSupportFragmentManager().beginTransaction()
-					.add(R.id.list, gauges).commit();
-		}
+					.add(R.id.list, new GaugeListFragment()).commit();
 	}
 
 }
