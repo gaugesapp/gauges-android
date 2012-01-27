1616b7e9344e60326d4fa0e3250f53c8ae66d897
Kevin Sawicki <kevin@github.com>
2012-01-26 16:22:40 -0800

    Draw 7 day bar chart on gauge list view

--- src/main/java/com/github/mobile/gauges/ui/GaugeViewHolder.java
+++ src/main/java/com/github/mobile/gauges/ui/GaugeViewHolder.java
@@ -1,9 +1,11 @@
 package com.github.mobile.gauges.ui;
 
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.mobile.gauges.R.id;
+import com.github.mobile.gauges.core.DatedViewSummary;
 import com.github.mobile.gauges.core.Gauge;
 import com.madgag.android.listviews.ViewHolder;
 
@@ -20,15 +22,18 @@ public class GaugeViewHolder implements ViewHolder<Gauge> {
 
 	private final TextView peopleText;
 
+	private final LinearLayout barGraph;
+
 	/**
 	 * Create view holder
-	 * 
+	 *
 	 * @param view
 	 */
 	public GaugeViewHolder(final View view) {
 		nameText = (TextView) view.findViewById(id.tv_gauge_name);
 		viewsText = (TextView) view.findViewById(id.tv_gauge_views);
 		peopleText = (TextView) view.findViewById(id.tv_gauge_people);
+		barGraph = (LinearLayout) view.findViewById(id.ll_bars);
 	}
 
 	public void updateViewFor(final Gauge gauge) {
@@ -37,6 +42,13 @@ public class GaugeViewHolder implements ViewHolder<Gauge> {
 				.getViews()));
 		peopleText.setText(MessageFormat.format("{0}", gauge.getToday()
 				.getPeople()));
+		DatedViewSummary[] lastWeek = new DatedViewSummary[7];
+		int index = lastWeek.length - 1;
+		for (DatedViewSummary day : gauge.getRecentDays()) {
+			lastWeek[index--] = day;
+			if (index < 0)
+				break;
+		}
+		barGraph.setBackgroundDrawable(new LastWeekGraphDrawable(lastWeek));
 	}
-
 }
