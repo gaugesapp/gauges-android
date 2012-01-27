9ebed5395d0d33e3be61bdbae9109fc288d7440f
Kevin Sawicki <kevin@github.com>
2012-01-27 15:50:16 -0800

    Rename inner class adapter to GaugePagerAdapter
    Previously was called PagerAdapter which was matching a superclass

--- src/main/java/com/github/mobile/gauges/ui/GaugeViewActivity.java
+++ src/main/java/com/github/mobile/gauges/ui/GaugeViewActivity.java
@@ -24,89 +24,86 @@ import roboguice.activity.RoboFragmentActivity;
  */
 public class GaugeViewActivity extends RoboFragmentActivity {
 
-	/**
-	 * Pager adapter for a gauge
-	 */
-	public static class PagerAdapter extends FragmentPagerAdapter implements
-			TitleProvider {
+    /**
+     * Pager adapter for a gauge
+     */
+    public static class GaugePagerAdapter extends FragmentPagerAdapter implements TitleProvider {
 
-		private final Context context;
+        private final Context context;
 
-		private final Gauge gauge;
+        private final Gauge gauge;
 
-		/**
-		 * Create pager adapter
-		 *
-		 * @param context
-		 *
-		 * @param gauge
-		 * @param fragmentManager
-		 */
-		public PagerAdapter(Context context, Gauge gauge,
-				FragmentManager fragmentManager) {
-			super(fragmentManager);
-			this.context = context;
-			this.gauge = gauge;
-		}
+        /**
+         * Create pager adapter
+         *
+         * @param context
+         *
+         * @param gauge
+         * @param fragmentManager
+         */
+        public GaugePagerAdapter(Context context, Gauge gauge, FragmentManager fragmentManager) {
+            super(fragmentManager);
+            this.context = context;
+            this.gauge = gauge;
+        }
 
-		@Override
-		public int getCount() {
-			return 3;
-		}
+        @Override
+        public int getCount() {
+            return 3;
+        }
 
-		@Override
-		public Fragment getItem(int position) {
-			Bundle bundle = new Bundle();
-			bundle.putSerializable(GAUGE, gauge);
-			bundle.putString(GAUGE_ID, gauge.getId());
-			switch (position) {
-			case 0:
-				ContentListFragment contentFragment = new ContentListFragment();
-				contentFragment.setArguments(bundle);
-				return contentFragment;
-			case 1:
-				TrafficListFragment trafficFragment = new TrafficListFragment();
-				trafficFragment.setArguments(bundle);
-				return trafficFragment;
-			case 2:
-				ReferrerListFragment referrerFragment = new ReferrerListFragment();
-				referrerFragment.setArguments(bundle);
-				return referrerFragment;
-			default:
-				return null;
-			}
-		}
+        @Override
+        public Fragment getItem(int position) {
+            Bundle bundle = new Bundle();
+            bundle.putSerializable(GAUGE, gauge);
+            bundle.putString(GAUGE_ID, gauge.getId());
+            switch (position) {
+            case 0:
+                ContentListFragment contentFragment = new ContentListFragment();
+                contentFragment.setArguments(bundle);
+                return contentFragment;
+            case 1:
+                TrafficListFragment trafficFragment = new TrafficListFragment();
+                trafficFragment.setArguments(bundle);
+                return trafficFragment;
+            case 2:
+                ReferrerListFragment referrerFragment = new ReferrerListFragment();
+                referrerFragment.setArguments(bundle);
+                return referrerFragment;
+            default:
+                return null;
+            }
+        }
 
-		public String getTitle(int position) {
-			switch (position) {
-			case 0:
-				return context.getString(string.page_content);
-			case 1:
-				return context.getString(string.page_traffic);
-			case 2:
-				return context.getString(string.page_referrers);
-			default:
-				return null;
-			}
-		}
-	}
+        public String getTitle(int position) {
+            switch (position) {
+            case 0:
+                return context.getString(string.page_content);
+            case 1:
+                return context.getString(string.page_traffic);
+            case 2:
+                return context.getString(string.page_referrers);
+            default:
+                return null;
+            }
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.gauge_view);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gauge_view);
 
-		Gauge gauge = (Gauge) getIntent().getSerializableExtra(GAUGE);
-		setTitle(gauge.getTitle());
+        Gauge gauge = (Gauge) getIntent().getSerializableExtra(GAUGE);
+        setTitle(gauge.getTitle());
 
-		ViewPager pager = (ViewPager) findViewById(id.vp_pages);
-		pager.setAdapter(new PagerAdapter(getApplicationContext(), gauge,
-				getSupportFragmentManager()));
+        ViewPager pager = (ViewPager) findViewById(id.vp_pages);
+        pager.setAdapter(new GaugePagerAdapter(getApplicationContext(), gauge, getSupportFragmentManager()));
 
-		TitlePageIndicator indicator = (TitlePageIndicator) findViewById(id.tpi_header);
-		indicator.setTextColor(getResources().getColor(color.text_light));
-		indicator.setFooterColor(getResources().getColor(color.text_light));
-		indicator.setViewPager(pager);
-		pager.setCurrentItem(1);
-	}
+        TitlePageIndicator indicator = (TitlePageIndicator) findViewById(id.tpi_header);
+        indicator.setTextColor(getResources().getColor(color.text_light));
+        indicator.setFooterColor(getResources().getColor(color.text_light));
+        indicator.setViewPager(pager);
+        pager.setCurrentItem(1);
+    }
 }
