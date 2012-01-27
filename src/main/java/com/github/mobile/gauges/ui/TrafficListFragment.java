5ea09331e9c65c77842ef46c4dc5059e7b9a76d2
Kevin Sawicki <kevin@github.com>
2012-01-26 17:19:32 -0800

    Add bar graph of last month's views and people

--- src/main/java/com/github/mobile/gauges/ui/TrafficListFragment.java
+++ src/main/java/com/github/mobile/gauges/ui/TrafficListFragment.java
@@ -3,13 +3,17 @@ package com.github.mobile.gauges.ui;
 import static com.github.mobile.gauges.IntentConstants.GAUGE;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.View;
 
+import com.github.mobile.gauges.R.id;
 import com.github.mobile.gauges.R.layout;
 import com.github.mobile.gauges.core.DatedViewSummary;
 import com.github.mobile.gauges.core.Gauge;
 import com.madgag.android.listviews.ReflectiveHolderFactory;
 import com.madgag.android.listviews.ViewHoldingListAdapter;
 import com.madgag.android.listviews.ViewInflator;
+
+import java.util.List;
 
 /**
  * Fragment to display list of recent traffic
@@ -19,10 +23,28 @@ public class TrafficListFragment extends ListFragment {
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		setListAdapter(new ViewHoldingListAdapter<DatedViewSummary>(
-				((Gauge) getArguments().getSerializable(GAUGE)).getRecentDays(),
+
+		Gauge gauge = (Gauge) getArguments().getSerializable(GAUGE);
+		List<DatedViewSummary> recentDays = gauge.getRecentDays();
+
+		View view = getLayoutInflater(savedInstanceState).inflate(
+				layout.traffic_graph, null);
+		DatedViewSummary[] graphDays = recentDays
+				.toArray(new DatedViewSummary[recentDays.size()]);
+		for (int i = 0; i < graphDays.length / 2; i++) {
+			DatedViewSummary swap = graphDays[i];
+			graphDays[i] = graphDays[graphDays.length - 1 - i];
+			graphDays[graphDays.length - 1 - i] = swap;
+		}
+
+		(view.findViewById(id.ll_bars))
+				.setBackgroundDrawable(new LastMonthGraphDrawable(graphDays));
+		getListView().addHeaderView(view);
+
+		setListAdapter(new ViewHoldingListAdapter<DatedViewSummary>(recentDays,
 				ViewInflator.viewInflatorFor(getActivity(),
-						layout.traffic_list_item), ReflectiveHolderFactory
+						layout.traffic_list_item),
+				ReflectiveHolderFactory
 						.reflectiveFactoryFor(TrafficViewHolder.class)));
 	}
 }
