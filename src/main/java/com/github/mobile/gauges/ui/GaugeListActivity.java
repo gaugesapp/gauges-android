3870264247dce557ce304481d2ff0cdde794c4d9
Kevin Sawicki <kevin@github.com>
2012-01-29 10:26:38 -0800

    Format and remove extra whitespace
    No functional change

--- src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
+++ src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
@@ -16,33 +16,29 @@ import roboguice.activity.RoboFragmentActivity;
  */
 public class GaugeListActivity extends RoboFragmentActivity {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu optionsMenu) {
-		getMenuInflater().inflate(menu.gauges, optionsMenu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu optionsMenu) {
+        getMenuInflater().inflate(menu.gauges, optionsMenu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case id.refresh:
-			((GaugeListFragment) getSupportFragmentManager().findFragmentById(
-					R.id.list)).refresh();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case id.refresh:
+            ((GaugeListFragment) getSupportFragmentManager().findFragmentById(R.id.list)).refresh();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.gauge_list);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(layout.gauge_list);
-
-		if (getSupportFragmentManager().findFragmentById(R.id.list) == null)
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.list, new GaugeListFragment()).commit();
-	}
-
+        if (getSupportFragmentManager().findFragmentById(R.id.list) == null)
+            getSupportFragmentManager().beginTransaction().add(R.id.list, new GaugeListFragment()).commit();
+    }
 }
