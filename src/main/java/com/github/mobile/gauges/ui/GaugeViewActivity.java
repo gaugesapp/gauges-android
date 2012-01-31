804bb9c68dc02e035eea2529e6c3b0efea1295e3
Roberto Tyley <roberto@github.com>
2012-01-31 13:35:36 +0000

    Enable 'up' navigation on the app icon for the Gauge view activity
    ...shows the 'up' chevron (actually renders pointing leftwards) on the app
    icon, takes you back to the Gauge list activity.
    
    http://developer.android.com/guide/topics/ui/actionbar.html#Up
    
    Tested on Gingerbread and ICS

--- src/main/java/com/github/mobile/gauges/ui/GaugeViewActivity.java
+++ src/main/java/com/github/mobile/gauges/ui/GaugeViewActivity.java
@@ -1,12 +1,15 @@
 package com.github.mobile.gauges.ui;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
+import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import static com.github.mobile.gauges.IntentConstants.GAUGE;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItem;
 import android.support.v4.view.ViewPager;
 
 import com.github.mobile.gauges.R.id;
 import com.github.mobile.gauges.R.layout;
-
 import com.github.mobile.gauges.core.Gauge;
 import com.viewpagerindicator.TitlePageIndicator;
 
@@ -25,6 +28,7 @@ public class GaugeViewActivity extends RoboFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.gauge_view);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Gauge gauge = (Gauge) getIntent().getSerializableExtra(GAUGE);
         setTitle(gauge.getTitle());
@@ -33,5 +37,18 @@ public class GaugeViewActivity extends RoboFragmentActivity {
 
         indicator.setViewPager(pager);
         pager.setCurrentItem(1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, GaugeListActivity.class);
+                intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP|FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
