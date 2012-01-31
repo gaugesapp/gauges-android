f2994d312ade682ef73c278efdabc71cd189c16f
Kevin Sawicki <kevin@github.com>
2012-01-31 08:21:30 -0800

    Disable http.keepAlive on Froyo and below
    Without this set about 50% of the requests return a -1 response
    code causing lists to appear empty when viewing gauge information

--- src/main/java/com/github/mobile/gauges/GaugesApplication.java
+++ src/main/java/com/github/mobile/gauges/GaugesApplication.java
@@ -1,35 +1,44 @@
 package com.github.mobile.gauges;
 
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.FROYO;
 import android.app.Application;
 import android.app.Instrumentation;
 import android.content.Context;
 
+import com.github.kevinsawicki.http.HttpRequest;
+
 /**
- * 
+ * Gauges application
  */
 public class GaugesApplication extends Application {
 
-	/**
-	 * Create main application
-	 */
-	public GaugesApplication() {
-	}
+    /**
+     * Create main application
+     */
+    public GaugesApplication() {
+        // Disable http.keepAlive on Froyo and below
+        if (SDK_INT <= FROYO)
+            HttpRequest.keepAlive(false);
+    }
 
-	/**
-	 * Create main application
-	 * 
-	 * @param context
-	 */
-	public GaugesApplication(final Context context) {
-		attachBaseContext(context);
-	}
+    /**
+     * Create main application
+     *
+     * @param context
+     */
+    public GaugesApplication(final Context context) {
+        this();
+        attachBaseContext(context);
+    }
 
-	/**
-	 * Create main application
-	 * 
-	 * @param instrumentation
-	 */
-	public GaugesApplication(final Instrumentation instrumentation) {
-		attachBaseContext(instrumentation.getTargetContext());
-	}
+    /**
+     * Create main application
+     *
+     * @param instrumentation
+     */
+    public GaugesApplication(final Instrumentation instrumentation) {
+        this();
+        attachBaseContext(instrumentation.getTargetContext());
+    }
 }
