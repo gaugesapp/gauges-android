afddf786f0f7c34425c6c471a3b95b5f81600cd5
Kevin Sawicki <kevin@github.com>
2012-02-08 16:07:00 -0800

    Show toast when list loading fails

--- app/src/main/java/com/github/mobile/gauges/ui/GaugeListLoader.java
+++ app/src/main/java/com/github/mobile/gauges/ui/GaugeListLoader.java
@@ -1,15 +1,17 @@
 package com.github.mobile.gauges.ui;
 
-import static java.util.Collections.emptyList;
 import android.accounts.AccountsException;
-import android.content.Context;
+import android.app.Activity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.github.mobile.gauges.GaugesServiceProvider;
+import com.github.mobile.gauges.R.string;
 import com.github.mobile.gauges.core.Gauge;
 import com.google.inject.Inject;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,18 +21,36 @@ public class GaugeListLoader extends AsyncLoader<List<Gauge>> {
 
     private final static String TAG = "GLL";
 
+    private final Activity activity;
+
     private final GaugesServiceProvider serviceProvider;
 
     /**
-     * Creates a gauge list loader using the given {@link Context} and {@link GaugesServiceProvider}
+     * Creates a gauge list loader using the given {@link Activity} and {@link GaugesServiceProvider}
      *
-     * @param context
+     * @param activity
      * @param serviceProvider
      */
     @Inject
-    public GaugeListLoader(final Context context, final GaugesServiceProvider serviceProvider) {
-        super(context);
+    public GaugeListLoader(final Activity activity, final GaugesServiceProvider serviceProvider) {
+        super(activity);
+        this.activity = activity;
         this.serviceProvider = serviceProvider;
+    }
+
+    /**
+     * Show {@link Toast} and log given exception
+     *
+     * @param e
+     */
+    protected void showError(final Exception e) {
+        Log.d(TAG, "Exception getting gauges", e);
+        activity.runOnUiThread(new Runnable() {
+
+            public void run() {
+                Toast.makeText(activity, activity.getString(string.error_loading_gauges), 5000).show();
+            }
+        });
     }
 
     @Override
@@ -38,10 +58,10 @@ public class GaugeListLoader extends AsyncLoader<List<Gauge>> {
         try {
             return serviceProvider.getService().getGauges();
         } catch (IOException e) {
-            Log.d(TAG, "Exception getting gauges", e);
+            showError(e);
         } catch (AccountsException e) {
-            Log.d(TAG, "Exception getting gauges", e);
+            showError(e);
         }
-        return emptyList();
+        return Collections.emptyList();
     }
 }
