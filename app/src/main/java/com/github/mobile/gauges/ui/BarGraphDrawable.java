17aaa5d2b1ed4d1d802c6ea5938e0f37ff4639ec
Kevin Sawicki <kevin@github.com>
2012-02-09 14:18:24 -0800

    Set background color on BarGraphDrawable to be transparent
    The gauge list items now have a gradient background and so the
    bar graph should draw no bounding box

--- app/src/main/java/com/github/mobile/gauges/ui/BarGraphDrawable.java
+++ app/src/main/java/com/github/mobile/gauges/ui/BarGraphDrawable.java
@@ -10,46 +10,45 @@ import android.graphics.drawable.PaintDrawable;
  */
 public class BarGraphDrawable extends PaintDrawable {
 
-	private static final int MIN_HEIGHT = 4;
+    private static final int MIN_HEIGHT = 4;
 
-	private static final int SPACING_X = 2;
+    private static final int SPACING_X = 2;
 
-	private final int[][] colors;
+    private final int[][] colors;
 
-	private final long[][] data;
+    private final long[][] data;
 
-	private long max = 1;
+    private long max = 1;
 
-	/**
-	 * Create drawable bar graph for data and colors
-	 *
-	 * @param data
-	 * @param colors
-	 */
-	public BarGraphDrawable(final long[][] data, final int[][] colors) {
-		this.data = data;
-		this.colors = colors;
-		for (int i = 0; i < data.length; i++)
-			for (int j = 0; j < data[i].length; j++)
-				max = Math.max(max, data[i][j]);
-	}
+    /**
+     * Create drawable bar graph for data and colors
+     *
+     * @param data
+     * @param colors
+     */
+    public BarGraphDrawable(final long[][] data, final int[][] colors) {
+        super(android.R.color.transparent);
+        this.data = data;
+        this.colors = colors;
+        for (int i = 0; i < data.length; i++)
+            for (int j = 0; j < data[i].length; j++)
+                max = Math.max(max, data[i][j]);
+    }
 
-	@Override
-	public void draw(final Canvas canvas) {
-		final Paint paint = getPaint();
-		final Rect bound = getBounds();
-		final int width = bound.width() / data.length - SPACING_X;
-		final int height = bound.height();
-		int x = 0;
-		for (int i = 0; i < data.length; i++) {
-			for (int j = 0; j < data[i].length; j++) {
-				paint.setColor(colors[i][j]);
-				float percentage = (float) data[i][j] / max;
-				canvas.drawRect(x,
-						height - Math.max(MIN_HEIGHT, percentage * height), x
-								+ width, bound.bottom, paint);
-			}
-			x += width + SPACING_X;
-		}
-	}
+    @Override
+    public void draw(final Canvas canvas) {
+        final Paint paint = getPaint();
+        final Rect bound = getBounds();
+        final int width = bound.width() / data.length - SPACING_X;
+        final int height = bound.height();
+        int x = 0;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                paint.setColor(colors[i][j]);
+                float percentage = (float) data[i][j] / max;
+                canvas.drawRect(x, height - Math.max(MIN_HEIGHT, percentage * height), x + width, bound.bottom, paint);
+            }
+            x += width + SPACING_X;
+        }
+    }
 }
