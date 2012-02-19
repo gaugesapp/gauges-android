fd89f8ef2905cc2d633e46b09d70437ab22f9d7a
Kevin Sawicki <kevin@github.com>
2012-02-19 14:17:58 -0800

    Launch gauge view activity from GaugeListFragment
    This was previously done by propagating the click event from
    the fragment to the activity so it would start the new activity
    to view the gauge.
    
    This removes the single use listener and still allows the activity
    to intercept the intent request from the fragment by overriding
    the startActivityFromFragment if needed.

--- app/src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
+++ app/src/main/java/com/github/mobile/gauges/ui/GaugeListActivity.java
@@ -1,30 +1,19 @@
 package com.github.mobile.gauges.ui;
 
-import static com.github.mobile.gauges.IntentConstants.GAUGE;
-import static com.github.mobile.gauges.IntentConstants.VIEW_GAUGE;
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.github.mobile.gauges.R.layout;
-import com.github.mobile.gauges.core.Gauge;
 
 import roboguice.activity.RoboFragmentActivity;
 
 /**
  * Activity to display list of gauge summaries
  */
-public class GaugeListActivity extends RoboFragmentActivity implements OnGaugeSelectedListener {
+public class GaugeListActivity extends RoboFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.gauge_list);
-    }
-
-    @Override
-    public void onGaugeSelected(Gauge gauge) {
-        Intent i = new Intent(VIEW_GAUGE);
-        i.putExtra(GAUGE, gauge);
-        startActivity(i);
     }
 }
