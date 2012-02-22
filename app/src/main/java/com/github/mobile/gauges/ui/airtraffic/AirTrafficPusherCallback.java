193791d3cd65109d3d6c554a7a16ad127071e8b4
Kevin Sawicki <kevin@github.com>
2012-02-21 18:42:23 -0800

    Guard against missing longitude, latitude and site ids
    Events where these fields are not set correctly should be
    dropped and not bubbled up as hits to display

--- app/src/main/java/com/github/mobile/gauges/ui/airtraffic/AirTrafficPusherCallback.java
+++ app/src/main/java/com/github/mobile/gauges/ui/airtraffic/AirTrafficPusherCallback.java
@@ -17,19 +17,15 @@
 package com.github.mobile.gauges.ui.airtraffic;
 
 import static org.json.JSONObject.NULL;
-import android.util.Log;
 
 import com.emorym.android_pusher.PusherCallback;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * Callback that delivers pushed air traffic events to an implemented {@link #onHit(Hit)} callback
  */
 public abstract class AirTrafficPusherCallback extends PusherCallback {
-
-    private static final String TAG = "ATPC";
 
     /**
      * Process hit
@@ -44,29 +40,29 @@ public abstract class AirTrafficPusherCallback extends PusherCallback {
      * @param data
      * @param key
      * @return value, may be null
-     * @throws JSONException
      */
-    protected String getString(final JSONObject data, final String key) throws JSONException {
-        final Object value = data.get(key);
-        if (value != null && value != NULL)
-            return value.toString();
-        else
-            return null;
+    protected String getString(final JSONObject data, final String key) {
+        final Object value = data.opt(key);
+        return value != null && value != NULL ? value.toString() : null;
     }
 
     @Override
     public void onEvent(final JSONObject eventData) {
-        try {
-            float longitude = (float) eventData.getDouble("longitude");
-            float latitude = (float) eventData.getDouble("latitude");
-            String id = getString(eventData, "site_id");
-            String city = getString(eventData, "city");
-            String region = getString(eventData, "region");
-            String country = getString(eventData, "country");
-            String title = getString(eventData, "title");
-            onHit(new Hit(id, title, longitude, latitude, city, region, country));
-        } catch (JSONException e) {
-            Log.d(TAG, "JSON exception", e);
-        }
+        double longitude = eventData.optDouble("longitude");
+        if (Double.isNaN(longitude))
+            return;
+        double latitude = eventData.optDouble("latitude");
+        if (Double.isNaN(latitude))
+            return;
+
+        String id = getString(eventData, "site_id");
+        if (id == null || id.length() == 0)
+            return;
+
+        String city = getString(eventData, "city");
+        String region = getString(eventData, "region");
+        String country = getString(eventData, "country");
+        String title = getString(eventData, "title");
+        onHit(new Hit(id, title, (float) longitude, (float) latitude, city, region, country));
     }
 }
