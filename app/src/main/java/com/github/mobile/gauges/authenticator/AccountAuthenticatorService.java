bc96849a1814d873bec39b230d136303441060c1
Kevin Sawicki <kevin@github.com>
2012-05-30 12:29:30 -0700

    Use all uppercase for static variable names

--- app/src/main/java/com/github/mobile/gauges/authenticator/AccountAuthenticatorService.java
+++ app/src/main/java/com/github/mobile/gauges/authenticator/AccountAuthenticatorService.java
@@ -25,16 +25,17 @@ import android.os.IBinder;
  * Authenticator service that returns a subclass of AbstractAccountAuthenticator in onBind()
  */
 public class AccountAuthenticatorService extends Service {
-    private static GaugesAccountAuthenticator sAccountAuthenticator = null;
 
+    private static GaugesAccountAuthenticator AUTHENTICATOR = null;
+
+    @Override
     public IBinder onBind(Intent intent) {
         return intent.getAction().equals(ACTION_AUTHENTICATOR_INTENT) ? getAuthenticator().getIBinder() : null;
     }
 
     private GaugesAccountAuthenticator getAuthenticator() {
-        if (sAccountAuthenticator == null)
-            sAccountAuthenticator = new GaugesAccountAuthenticator(this);
-        return sAccountAuthenticator;
+        if (AUTHENTICATOR == null)
+            AUTHENTICATOR = new GaugesAccountAuthenticator(this);
+        return AUTHENTICATOR;
     }
-
 }
